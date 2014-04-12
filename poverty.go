34917d9adbdb0b570181de7a8d10acc48ba94c24
Anthony Phelps <anthonypphelps@gmail.com>
2014-04-12 14:44:32 -0500

    Added budgeting

--- poverty.go
+++ poverty.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	//"fmt"
 	"github.com/go-martini/martini"
 	_"github.com/mattn/go-sqlite3"
 	"database/sql"
 	"net/http"
 	"encoding/json"
 	"strings"
+	"fmt"
 )
 
 type Data struct{
@@ -17,6 +17,12 @@ type Data struct{
 	Date int `json:"date"`
 	Amount float64 `json:"amount"`
 	Categories []string `json:"categories"`
+}
+
+type Budget struct{
+	Uname string `json:"uname"`
+	Cname string `json:"cname"`
+	Amount float64 `json:"amount"`
 }
 
 func main(){
@@ -33,6 +39,11 @@ func main(){
 	}
 
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS categories (tid INTEGER, cname TEXT)")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS budgets (uname TEXT, cname TEXT, amount REAL)")
 	if err != nil {
 		panic(err)
 	}
@@ -105,7 +116,89 @@ func main(){
 			}
 		}
 
-		return "meh"
+		return "probably added."
+	})
+
+	m.Get("/budget/add", func(params martini.Params, r *http.Request) string {
+		_, err = db.Exec("INSERT INTO budgets VALUES(?, ?, ?)", r.FormValue("uname"), r.FormValue("cname"), r.FormValue("amount"))
+		if err != nil {
+			panic(err)
+		}
+
+		return "probably added."
+	})
+
+	m.Get("/budget/get", func(params martini.Params, r *http.Request) string {
+		var resp string
+
+		if r.FormValue("all") == "true" {
+			rows, err := db.Query("SELECT cname, amount FROM budgets WHERE uname = ?", r.FormValue("uname"))
+			if err != nil {
+				panic(err)
+			}
+			defer rows.Close()
+
+			buds := make([]Budget, 0, 5)
+
+			for rows.Next() {
+				var bud Budget
+				bud.Uname = r.FormValue("uname")
+
+				err = rows.Scan(&bud.Cname, &bud.Amount)
+				if err != nil {
+					panic(err)
+				}
+
+				buds = append(buds, bud)
+			}
+			rows.Close()
+
+			byt, err := json.Marshal(buds)
+			if err != nil {
+				panic(err)
+			}
+			resp = string(byt)
+
+		} else {
+			rows, err := db.Query("SELECT amount FROM budgets WHERE uname = ? AND cname = ?", r.FormValue("uname"), r.FormValue("cname"))
+			if err != nil {
+				panic(err)
+			}
+			defer rows.Close()
+
+			buds := make([]Budget, 0, 5)
+
+			for rows.Next() {
+				var bud Budget
+				bud.Uname = r.FormValue("uname")
+				bud.Cname = r.FormValue("cname")
+
+				err = rows.Scan(&bud.Amount)
+				fmt.Println(bud.Amount)
+				if err != nil {
+					panic(err)
+				}
+
+				buds = append(buds, bud)
+			}
+			rows.Close()
+
+			byt, err := json.Marshal(buds)
+			if err != nil {
+				panic(err)
+			}
+			resp = string(byt)
+		}
+			return string(resp)
+	})
+
+	m.Get("/budget/remove", func(params martini.Params, r *http.Request) string {
+		_, err = db.Exec("DELETE FROM budgets WHERE uname=? AND cname=?", r.FormValue("uname"), r.FormValue("category"))
+		if err != nil {
+			panic(err)
+		}
+
+		return "probably removed."
 	})
 
 	m.Run()
